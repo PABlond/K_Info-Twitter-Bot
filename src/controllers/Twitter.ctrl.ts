4fff6df1ef48816a28f368a37361c2b0aa45499b
pablond <pierre-alexis.blond@live.fr>
2020-05-24 22:22:31 +0200

    update src/controllers/Twitter.ctrl.ts

---
+++ src/controllers/Twitter.ctrl.ts
@@ -0,0 +1,44 @@
+import twitter from 'twitter'
+import { ITwitterConfig } from '../interfaces'
+
+export default class Twitter {
+    client: twitter
+    accountClient: twitter
+
+    constructor(config: {
+        client: ITwitterConfig
+        accountClient: ITwitterConfig
+    }) {
+        this.client = new twitter(config.client)
+        this.accountClient = new twitter(config.accountClient)
+    }
+
+    getTrends = async (): Promise<string[]> => {
+        const params = { id: 615702 }
+        const [{ trends }] = (await this.client.get(
+            'trends/place',
+            params
+        )) as any
+        return trends.map(({ name }: { name: string }) => name)
+    }
+
+    postTweet = async ({
+        trend,
+        text: status,
+        href: attachment_url,
+    }: {
+        trend: string
+        href: string
+        text: string
+    }): Promise<void> => {
+        const params = { status: status + ' ' + attachment_url }
+        try {
+            const data = await this.accountClient.post(
+                'statuses/update',
+                params
+            )
+        } catch (err) {
+            console.log(err)
+        }
+    }
+}
