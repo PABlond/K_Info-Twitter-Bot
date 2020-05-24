5a1a3b3c117f3ef19ea20de3dcf000a098f96536
pablond <pierre-alexis.blond@live.fr>
2020-05-24 22:22:31 +0200

    update src/models/article.model.ts

---
+++ src/models/article.model.ts
@@ -0,0 +1,11 @@
+import { Schema, model } from 'mongoose'
+
+export default model(
+    'article',
+    new Schema({
+        trend: String,
+        text: String,
+        href: String,
+        at: Date,
+    })
+)
