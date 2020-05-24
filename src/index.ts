db587054a62a8cc92a93832cf7ace6770102ba84
pablond <pierre-alexis.blond@live.fr>
2020-05-24 22:36:44 +0200

    update src/index.ts ======================================================= lhs: 100644 | 579fba2f0b51537e85ceb1439b0c5303a7e01744 rhs: None

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,10 @@ import twitterConfig from './config/twitter.config'
 import dbConnection from './config/db.config'
 
 dbConnection(async () => {
-    console.clear()
-    console.log('[ ] Connected to database')
-    const bot = new Bot(twitterConfig)
-    await bot.run()
+  console.clear()
+  console.log('[ ] Connected to database')
+  const bot = new Bot(twitterConfig)
+  await bot.run()
+  const interval = 3 * 60 * 1000 // 3min()
+  setInterval(bot.run, interval)
 })
