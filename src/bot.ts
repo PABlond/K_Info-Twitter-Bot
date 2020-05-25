a89c54785683b0612f810628cf97a6d7aae338c2
pablond <pierre-alexis.blond@live.fr>
2020-05-25 17:15:57 +0200

    update src/bot.ts ======================================================= lhs: 100644 | 50628b0f10dbfd581349b87a9d7bed903f8fd2be rhs: None

--- src/bot.ts
+++ src/bot.ts
@@ -3,7 +3,7 @@ import TweetModel from './models/tweet.model'
 import ArticleModel from './models/article.model'
 import Article from './controllers/Article.ctrl'
 import { IScrappedLinks, IRelations } from './interfaces'
-import { formatTrend } from './services'
+import { formatTrend, getProvider } from './services'
 
 export default class Bot {
     twitter: any
@@ -26,11 +26,29 @@ export default class Bot {
             if (!(await TweetModel.findOne({ href }))) {
                 console.log('\t-> Tweet article')
                 console.log(relation)
-                const article = (await ArticleModel.findOne({ href })) as any
-                article.target = true
-                await article.save()
-                await new TweetModel({ ...relation, at: Date.now() }).save()
-                await this.twitter.postTweet({ text, href, trend })
+                // const article = (await ArticleModel.findOne({ href })) as any
+                // article.target = true
+                // await article.save()
+                const { href } = relation
+                const source = await getProvider(href)
+                const { image, desc } = await this.article.getArticleData(source, href)
+                if (source) {
+                    await new TweetModel({
+                        ...relation,
+                        at: Date.now(),
+                        source, image, desc
+                    }).save()
+                    const issent = await this.twitter.postTweet({
+                        text,
+                        href,
+                        trend,
+                    })
+                    if (issent)
+                        await new TweetModel({
+                            ...relation,
+                            at: Date.now(),
+                        }).save()
+                }
             }
         }
     }
@@ -39,12 +57,12 @@ export default class Bot {
         console.log('\t-> Keep articles in connection with trendings topics')
         for await (const link of this.links) {
             const { text, href, source } = link
-            if (!(await ArticleModel.findOne({ href })))
-                await new ArticleModel({
-                    ...link,
-                    at: Date.now(),
-                    target: false,
-                }).save()
+            // if (!(await ArticleModel.findOne({ href })))
+            //     await new ArticleModel({
+            //         ...link,
+            //         at: Date.now(),
+            //         target: false,
+            //     }).save()
             this.trendsName.forEach((trend: string) =>
                 text.indexOf(formatTrend(trend)) !== -1
                     ? this.relations.push({ trend, text, href, source })
@@ -65,10 +83,11 @@ export default class Bot {
         this.trendsName = await this.twitter.getTrends()
         await this.scrapArticles()
         if (this.article.error) {
-            return console.log(this.article.error)
+            console.log(this.article.error)
         }
         await this.makeRelations()
         await this.saveAndTweet()
         console.log('[ ] End of script')
+        process.exit(1);
     }
 }
