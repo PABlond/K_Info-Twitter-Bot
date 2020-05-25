168a2a4d4b7417bb4fc9ae5c36c5dac900644213
pablond <pierre-alexis.blond@live.fr>
2020-05-25 10:12:38 +0200

    update src/bot.ts ======================================================= lhs: 100644 | 09a050460c13b7be9b2537db7eff72c2d3dcc47a rhs: None

--- src/bot.ts
+++ src/bot.ts
@@ -27,28 +27,25 @@ export default class Bot {
             if (!(await TweetModel.findOne({ href }))) {
                 console.log('\t-> Tweet article')
                 console.log(relation)
-                // const article = (await ArticleModel.findOne({ href })) as any
-                // article.target = true
-                // await article.save()
-                // await new TweetModel({ ...relation, at: Date.now() }).save()
-                // await this.twitter.postTweet({ text, href, trend })
+                const article = (await ArticleModel.findOne({ href })) as any
+                article.target = true
+                await article.save()
+                await new TweetModel({ ...relation, at: Date.now() }).save()
+                await this.twitter.postTweet({ text, href, trend })
             }
         }
     }
 
     makeRelations = async (): Promise<void> => {
         console.log('\t-> Keep articles in connection with trendings topics')
-        this.trendsName.forEach((trend: string) => {
-            console.log(formatTrend(trend))
-        })
         for await (const link of this.links) {
             const { text, href, source } = link
-            // if (!(await ArticleModel.findOne({ href })))
-            //     await new ArticleModel({
-            //         ...link,
-            //         at: Date.now(),
-            //         target: false,
-            //     }).save()
+            if (!(await ArticleModel.findOne({ href })))
+                await new ArticleModel({
+                    ...link,
+                    at: Date.now(),
+                    target: false,
+                }).save()
             this.trendsName.forEach((trend: string) =>
                 text.indexOf(formatTrend(trend)) !== -1
                     ? this.relations.push({ trend, text, href, source })
